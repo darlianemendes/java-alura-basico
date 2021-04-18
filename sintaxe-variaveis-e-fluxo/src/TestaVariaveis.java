57a3fdb2283e8a74532cba2f400c6bc63e8d2b4c
darlianemendes <contato.darlianemendes@outlook.com>
2021-04-18 18:56:21 -0300

    Removendo projeto

--- sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
+++
@@ -1,9 +0,0 @@
-
-public class TestaVariaveis {
-
-	public static void main(String[] args) {
-		System.out.println("Olá Novos Testes!");
-		int idade = 27;
-				System.out.println("Olá, minha idade é " + idade + ". Parabéns!");
-	}
-}
