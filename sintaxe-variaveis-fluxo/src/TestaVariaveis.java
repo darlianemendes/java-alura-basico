12aa31cba2f0189c206283129f4aeca018ac836f
darlianemendes <contato.darlianemendes@outlook.com>
2021-04-18 18:54:21 -0300

    ajuste

--- sintaxe-variaveis-fluxo/src/TestaVariaveis.java
+++ sintaxe-variaveis-fluxo/src/TestaVariaveis.java
@@ -3,6 +3,7 @@ public class TestaVariaveis {
 
 	public static void main(String[] args) {
 		System.out.println("Olá Novos Testes!");
+		
 		int idade = 27;
 				System.out.println("Olá, minha idade é " + idade + ". Parabéns!");
 	}
