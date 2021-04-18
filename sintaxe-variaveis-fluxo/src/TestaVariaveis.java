76034730ede1081428947ed8596d146ee7521795
darlianemendes <contato.darlianemendes@outlook.com>
2021-04-18 18:52:54 -0300

    Correção de encoding

--- sintaxe-variaveis-fluxo/src/TestaVariaveis.java
+++ sintaxe-variaveis-fluxo/src/TestaVariaveis.java
@@ -2,8 +2,8 @@
 public class TestaVariaveis {
 
 	public static void main(String[] args) {
-		System.out.println("Olá Novos Testes!");
+		System.out.println("OlÃ¡ Novos Testes!");
 		int idade = 27;
-				System.out.println("Olá, minha idade é " + idade + ". Parabéns!");
+				System.out.println("OlÃ¡, minha idade Ã© " + idade + ". ParabÃ©ns!");
 	}
 }
