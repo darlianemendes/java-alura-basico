49791fe8d783e2f2decd5506ff52abf4e8c29e72
darlianemendes <contato.darlianemendes@outlook.com>
2021-04-18 18:50:35 -0300

    Organizacao projeto

---
+++ sintaxe-variaveis-fluxo/src/TestaConversao.java
@@ -0,0 +1,16 @@
+public class TestaConversao{
+
+    public static void main(String[] args){
+
+        float pontoFlutuante = 3.14f; //não deu certo.Ver proximas aulas.
+        double salario = 1270.50;
+        int valor = (int) salario;
+        System.out.println(valor);
+
+        double valor1 = 0.2f;
+        double valor2 = 0.1f;
+        double total = valor1 + valor2;
+
+        System.out.println(total);
+    }
+}
---
+++ sintaxe-variaveis-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,9 @@
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+		System.out.println("Olá Novos Testes!");
+		int idade = 27;
+				System.out.println("Olá, minha idade é " + idade + ". Parabéns!");
+	}
+}
